396a1a127fee05b0e6fd3806c5657e18bba1d01b
Gideon Rosales <gideonrosales@gmail.com>
2014-11-11 16:43:47 +0800

    postinstall.sh fix

--- postinstall.sh
+++ postinstall.sh
@@ -2,11 +2,11 @@
 
 git clone git@github.com:School-Improvement-Network/grunt-common.git grunt
 
-ln -sf ./node_modules/sinet-coding-convetions/.editorconfig .editorconfig
-ln -sf ./node_modules/sinet-coding-convetions/.eslintrc .eslintrc
-ln -sf ./node_modules/sinet-coding-convetions/.gitattributes .gitattributes
-ln -sf ./node_modules/sinet-coding-convetions/.jscsrc .jscsrc
-ln -sf ./node_modules/sinet-coding-convetions/.jshintrc .jshintrc
+ln -sf ./node_modules/sinet-coding-conventions/.editorconfig .editorconfig
+ln -sf ./node_modules/sinet-coding-conventions/.eslintrc .eslintrc
+ln -sf ./node_modules/sinet-coding-conventions/.gitattributes .gitattributes
+ln -sf ./node_modules/sinet-coding-conventions/.jscsrc .jscsrc
+ln -sf ./node_modules/sinet-coding-conventions/.jshintrc .jshintrc
 
 cp ./module-initializer/GruntFile.js .
 ln -sf ./module-initializer/grunt ./grunt
