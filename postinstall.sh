dd07fbd2807b0a30e1f326b119753b69995341f4
Gideon Rosales <gideonrosales@gmail.com>
2014-11-11 15:46:03 +0800

    fix: postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+git submodule add git@github.com:School-Improvement-Network/grunt-common.git grunt
+
+ln -s ./node_modules/sinet-coding-convetion/.editorconfig .editorconfig
+ln -s ./node_modules/sinet-coding-convetion/.eslintrc .eslintrc
+ln -s ./node_modules/sinet-coding-convetion/.gitattributes .gitattributes
+ln -s ./node_modules/sinet-coding-convetion/.jscsrc .jscsrc
+ln -s ./node_modules/sinet-coding-convetion/.jshintrc .jshintrc
+
+cp GruntFile.js .
--- presinstall.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-git submodule add git@github.com:School-Improvement-Network/grunt-common.git grunt
-
-ln -s ./node_modules/sinet-coding-convetion/.editorconfig .editorconfig
-ln -s ./node_modules/sinet-coding-convetion/.eslintrc .eslintrc
-ln -s ./node_modules/sinet-coding-convetion/.gitattributes .gitattributes
-ln -s ./node_modules/sinet-coding-convetion/.jscsrc .jscsrc
-ln -s ./node_modules/sinet-coding-convetion/.jshintrc .jshintrc
